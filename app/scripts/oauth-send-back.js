327317508fe17c98adc89b6a5383f2696a7db464
JMPerez <jmperez1985@gmail.com>
2014-08-15 16:23:06 +0200

    Include missing script in callback

---
+++ app/scripts/oauth-send-back.js
@@ -0,0 +1,8 @@
+/*global OAuthConfig*/
+var target = window.self === window.top ? window.opener : window.parent;
+
+var hash = window.location.hash;
+if (hash) {
+    var token = window.location.hash.split('&')[0].split('=')[1];
+    target.postMessage(token, OAuthConfig.host);
+}
