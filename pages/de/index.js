8d0b97cc24f2b5eb9f26af70bb33b6f183a9e215
Jose M. Perez <JMPerez@users.noreply.github.com>
2020-04-05 22:43:38 +0200

    Update German translation

--- pages/de/index.js
+++ pages/de/index.js
@@ -9,25 +9,25 @@ export default () => {
     'menu.link-home': 'Home',
     'home.title': 'Spotify Deduplicator',
     'home.description':
-      'Entferne doppelte Musiktitel aus deinen Playlists und deiner Bibliothek.',
+      'Entferne doppelte Songs aus deinen Playlists und deinen Lieblingssongs.',
     'home.login-button': 'Mit Spotify einloggen',
     'meta.title':
       'Spotify Dedup - Entferne doppelte Musiktitel automatisch aus deiner Spotify-Bibliothek',
     'meta.description':
-      'Lösche schnell und einfach doppelte Songs aus deinen Spotify-Playlists und gespeicherten Titeln.',
-    'features.find-remove.header': 'Suchen und entfernen',
+      'Lösche schnell und einfach doppelte Songs aus deinen Spotify-Playlists und Lieblingssongs.',
+    'features.find-remove.header': 'Finden und löschen',
     'features.find-remove.body':
-      'Dedup überprüft deine Playlists und speichert Titel in deiner {{- strongOpen}}Spotify-Bibliothek{{- strongClose}}. Sobald Dedup Duplikate gefunden hat, kannst du diese pro Playlist entfernen.',
+      'Dedup überprüft deine Playlists und Lieblingssongs in deiner {{- strongOpen}}Spotify-Bibliothek{{- strongClose}}. Sobald Dedup Duplikate gefunden hat, kannst du diese pro Playlist entfernen.',
     'features.safer.header': 'Sicher',
     'features.safer.body':
-      'Dedup entfernt nur {{- strongOpen}}duplizierte Songs{{- strongClose}} und lässt den Rest der Playlist und die gespeicherten Titel unberührt.',
+      'Dedup entfernt nur {{- strongOpen}}doppelte Songs{{- strongClose}} und lässt den Rest der Playlist und die Lieblingssongs unberührt.',
     'features.open-source.header': 'Open Source',
     'features.open-source.body':
-      'Vielleicht möchten Sie sich den {{- linkGithubOpen}}Quellcode auf GitHub{{- linkGithubClose}} ansehen. Diese Webanwendung verwendet die {{- linkWebApiOpen}}Spotify Web API{{- linkWebApiClose}}, um die Playlists und gespeicherten Titel der Benutzer zu verwalten.',
-    'reviews.title': 'Das sagen die Benutzer',
+      'Vielleicht möchtest du dir den {{- linkGithubOpen}}Quellcode auf GitHub{{- linkGithubClose}} ansehen. Diese Webanwendung verwendet die {{- linkWebApiOpen}}Spotify Web API{{- linkWebApiClose}}, um die Playlists und Lieblingssongs der Nutzer zu verwalten.',
+    'reviews.title': 'Das sagen die Nutzer',
     'footer.author':
-      'Mit ♥ von {{- linkOpen}}JMPerez{{- linkClose}} gemacht 👨‍💻',
-    'footer.github': '{{- linkOpen}}Code auf GitHub{{- linkClose}} besuchen 📃',
+      'Mit ♥ gemacht von {{- linkOpen}}JMPerez 👨‍💻{{- linkClose}}',
+    'footer.github': '{{- linkOpen}}Code auf GitHub{{- linkClose}} ansehen 📃',
     'footer.bmc':
       'Unterstütze das Projekt {{- linkOpen}}beim Kaffeekauf ☕{{- linkClose}}',
     'bmc.button': 'Würdest du mir einen Kaffee kaufen?',
@@ -37,21 +37,21 @@ export default () => {
     'result.duplicate.track':
       '<0>{{trackName}}</0> <2>von</2> <4>{{trackArtistName}}</4>',
     'process.status.finding':
-      'Suche nach Duplikaten in deinen Playlists und gespeicherten Titeln…',
+      'Suche nach doppelten Titeln in deinen Playlists und Lieblingssongs…',
     'process.status.complete': 'Bearbeitung abgeschlossen!',
     'process.status.complete.body':
-      'Deine Playlists und gespeicherten Songs wurden verarbeitet!',
+      'Deine Playlists und Lieblingssongs wurden verarbeitet!',
     'process.status.complete.dups.body':
-      'Klicke auf die Schaltfläche {{- strongOpen}}Duplikate entfernen{{- strongClose}}, um Duplikate aus der entsprechenden Playlist oder Sammlung gespeicherter Titel zu entfernen.',
+      'Klicke auf die Schaltfläche {{- strongOpen}}Duplikate entfernen{{- strongClose}}, um Duplikate aus der entsprechenden Playlist oder deinen Lieblingssongs zu entfernen.',
     'process.status.complete.nodups.body':
-      'Glückwunsch! Du hast weder Duplikate in deinen Playlists noch in deinen gespeicherten Titeln.',
+      'Glückwunsch! Du hast weder Duplikate in deinen Playlists noch in deinen Lieblingssongs.',
     'process.reading-library':
-      'Deine Bibliothek wird nach Duplikaten in Playlists und deinen gespeicherten Songs durchsucht…',
+      'Deine Bibliothek wird nach Duplikaten in Playlists und deinen Lieblingssongs durchsucht…',
     'process.processing':
       'Suche nach doppelten Titeln. Bitte warte ein wenig. Es ist noch {{count}} Playlist zu verarbeiten…',
     'process.processing_plural':
       'Suche nach doppelten Titeln. Bitte warte ein wenig. Es sind noch {{count}} Playlists zu verarbeiten…',
-    'process.saved.title': 'Gespeicherte Songs in deiner Bibliothek',
+    'process.saved.title': 'Lieblingssongs in deiner Bibliothek',
     'process.saved.duplicates':
       'Diese Sammlung enthält {{count}} doppelten Song',
     'process.saved.duplicates_plural':
