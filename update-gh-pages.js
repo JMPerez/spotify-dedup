22e47202e845494fdb75abf71ce39dcae79bee44
Jose M. Perez <JMPerez@users.noreply.github.com>
2020-05-04 22:14:38 +0200

    Upgrade deps

--- update-gh-pages.js
+++ update-gh-pages.js
@@ -3,6 +3,6 @@ ghpages.publish('out', { dotfiles: true }, function(err) {
   if (err) {
     console.error('There was an error deploying gh-pages');
   } else {
-    console.log('Deplyment successful!');
+    console.log('Deployment successful!');
   }
 });
