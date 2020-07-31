e28a42f8c189d9e67d1b08df7a6284ebde82f9aa
paulvissers97 <69036835+paulvissers97@users.noreply.github.com>
2020-07-31 11:48:13 +0200

    Update index.js
    Made some changes to the translation. Everything was at least okay-ish, so mostly minor changes to make everything sound a bit more natural. If you have any questions, let me know.

--- pages/nl/index.js
+++ pages/nl/index.js
@@ -12,12 +12,12 @@ export default () => {
       'Verwijder dubbele nummers uit je afspeellijsten en opgeslagen nummers.',
     'home.login-button': 'Log in met Spotify',
     'meta.title':
-      'Spotify Dedup - Verwijder automatisch dubbele nummers uit uw Spotify-bibliotheek',
+      'Spotify Dedup - Verwijder automatisch dubbele nummers uit je Spotify-bibliotheek',
     'meta.description':
-      'Verwijder snel en gemakkelijk herhaalde nummers uit uw Spotify-afspeellijsten en opgeslagen nummers.',
+      'Verwijder snel en gemakkelijk herhaalde nummers uit je Spotify-afspeellijsten en opgeslagen nummers.',
     'features.find-remove.header': 'Zoeken en verwijderen',
     'features.find-remove.body':
-      'Dedup controleert je afspeellijsten en opgeslagen nummers in {{- strongOpen}}je Spotify-bibliotheek{{- strongClose}}. Zodra Dedup duplicaten heeft gevonden, kunt u ze per afspeellijst verwijderen.',
+      'Dedup controleert je afspeellijsten en opgeslagen nummers in {{- strongOpen}}je Spotify-bibliotheek{{- strongClose}}. Zodra Dedup duplicaten heeft gevonden, kun je ze per afspeellijst verwijderen.',
     'features.safer.header': 'Veiliger',
     'features.safer.body':
       'Dedup verwijdert alleen {{- strongOpen}}dubbele nummers{{- strongClose}}, waardoor de rest van de afspeellijst en opgeslagen nummers onaangetast blijven.',
@@ -29,35 +29,35 @@ export default () => {
       'Gemaakt met ♥ door {{- linkOpen}}JMPerez 👨‍💻{{- linkClose}}',
     'footer.github': 'Bekijk de {{- linkOpen}}code op GitHub 📃{{- linkClose}}',
     'footer.bmc':
-      'Steun het project {{- linkOpen}}om koffie te kopen ☕{{- linkClose}}',
+      'Steun het project door {{- linkOpen}}een koffietje voor me te kopen ☕{{- linkClose}}',
     'bmc.button': 'Zou je koffie voor me willen kopen?',
-    'result.duplicate.reason-same-id': 'Dupliceren',
+    'result.duplicate.reason-same-id': 'Duplicaat',
     'result.duplicate.reason-same-data':
-      'Dupliceren (zelfde naam, artiest en duur)',
+      'Duplicaat (zelfde naam, artiest en duur)',
     'result.duplicate.track':
       '<0>{{trackName}}</0> <2>door</2> <4>{{trackArtistName}}</4>',
     'process.status.finding':
-      'Het vinden van duplicaten in je afspeellijsten en opgeslagen nummers…',
+      'Er wordt gezocht naar duplicaten in je afspeellijsten en opgeslagen nummers…',
     'process.status.complete': 'Verwerking voltooid!',
     'process.status.complete.body':
       'Je afspeellijsten en opgeslagen nummers zijn verwerkt!',
     'process.status.complete.dups.body':
-      'Klik op de knop {{- strongOpen}}Verwijder duplicaten{{- strongClose}} om duplicaten in die afspeellijst of verzameling van opgeslagen nummers te verwijderen.',
+      'Klik op de knop {{- strongOpen}}Verwijder duplicaten{{- strongClose}} om duplicaten uit die afspeellijst of uit je opgeslagen nummers te verwijderen.',
     'process.status.complete.nodups.body':
-      'Gefeliciteerd! Je hebt geen duplicaten in je afspeellijsten noch opgeslagen nummers.',
+      'Gefeliciteerd! Je hebt geen duplicaten in je afspeellijsten en/of opgeslagen nummers.',
     'process.reading-library':
-      'Je bibliotheek doorzoeken, de afspeellijsten vinden die je bezit en je opgeslagen nummers…',
+      'Er wordt gezocht naar de afspeellijsten en opgeslagen nummers in je bibliotheek…',
     'process.processing':
-      'Wacht even op dubbele nummers. Nog {{count}} afspeellijst verwerken…',
+      'Er wordt gezocht naar duplicaten. Nog {{count}} afspeellijst te gaan…',
     'process.processing_plural':
-      'Wacht even op dubbele nummers. Nog {{count}} afspeellijsten verwerken…',
+      'Er wordt gezocht naar duplicaten. Nog {{count}} afspeellijsten te gaan…',
     'process.saved.title': 'Opgeslagen nummers in je bibliotheek',
     'process.saved.duplicates':
       'Deze verzameling heeft {{count}} dubbel nummer',
     'process.saved.duplicates_plural':
       'Deze verzameling heeft {{count}} dubbele nummers',
     'process.saved.remove-button':
-      'Verwijder duplicaten van je opgeslagen nummers',
+      'Verwijder duplicaten uit je opgeslagen nummers',
     'process.playlist.duplicates':
       'Deze afspeellijst heeft {{count}} dubbel nummer',
     'process.playlist.duplicates_plural':
