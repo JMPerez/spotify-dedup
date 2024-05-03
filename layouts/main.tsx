9abcc19be0b1cafde0df19b673a7c21b343d4379
José M. Pérez <jmperez1985@gmail.com>
2024-05-04 00:57:09 +0200

    Update text in notice

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -5,7 +5,7 @@ const Main = ({ children }) => (
     <Link className="bg-indigo-900 text-center py-4 lg:px-4 block" href="https://buymeacoffee.com/jmp/dedup-spotify-breaking-changes-api" target="_blank">
       <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
         <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">Info</span>
-        <span className="mr-2 text-left text-sm flex-auto text-balance">We are having some issues removing duplicates due to changes in Spotify's API. <span className="text-yellow-400 hover:text-yellow-200">Read more about it</span>.</span>
+        <span className="mr-2 text-left text-sm flex-auto text-balance">We have just fixed an issue that prevented the removal of duplicates in playlists. <span className="text-yellow-400 hover:text-yellow-200">Read more about it</span>.</span>
         <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" /></svg>
       </div>
     </Link>
