c801732b1d54f1511be2806ab9645ede70587250
José M. Pérez <jmperez1985@gmail.com>
2024-05-03 21:45:42 +0200

    Add notice about issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+    './layouts/**/*.{ts,tsx}',
+  ],
   theme: {
     container: {
       center: true,
