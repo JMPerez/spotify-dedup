4a4961d064b5ea6a987091cf0af7fbc2c2b180de
José M. Pérez <JMPerez@users.noreply.github.com>
2024-06-19 23:19:45 +0200

    Remove analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 
-import { Analytics } from '@vercel/analytics/react';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: any) {
@@ -8,11 +7,5 @@ export default function App({ Component, pageProps }: any) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics beforeSend={(event) => {
-        if (event.url.includes('/callback')) {
-          return null;
-        }
-        return event;
-      }} />
     </>);
 }
