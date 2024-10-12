215172c0614f843f43440df83a21475549cff98e
José M. Pérez <JMPerez@users.noreply.github.com>
2024-10-12 08:49:54 +0200

    Add Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: any) {
@@ -7,5 +8,6 @@ export default function App({ Component, pageProps }: any) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>);
 }
