b170409641e839f454c72c0572f737777b647f89
José M. Pérez <JMPerez@users.noreply.github.com>
2024-11-03 21:23:46 +0100

    Install Simple Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,18 @@
 import '../styles/globals.css';
 
-import { Analytics } from '@vercel/analytics/react';
-import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: any) {
-  const router = useRouter();
+  const pathname = usePathname()
+  const excludedPages = ['/callback']
+  const shouldTrack = !excludedPages.some(page =>
+    pathname !== null && (pathname === page || pathname.startsWith(page + '/')
+    ));
   return (
     <>
       <Component {...pageProps} />
-      <Analytics
-        beforeSend={(event) => {
-          if (event.url.includes('/callback')) {
-            return null;
-          }
-          return event;
-        }} />
+      {shouldTrack && <Script src="https://scripts.simpleanalyticscdn.com/latest.js"
+        data-ignore-pages="/callback" />}
     </>);
 }
