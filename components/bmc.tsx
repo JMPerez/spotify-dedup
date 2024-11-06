d912befacf78b78043bb659b3dc4579be66f0984
José M. Pérez <JMPerez@users.noreply.github.com>
2024-11-06 22:38:36 +0100

    Improve BMC

--- components/bmc.tsx
+++ components/bmc.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 
 const Bmc = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   return (
-    <div style={{ paddingTop: '1em' }}>
+    <div className="pt-4">
       <Link
-        className="bmc-button"
+        className="inline-flex items-center h-9 px-2 text-white bg-orange-500 rounded border border-transparent no-underline"
         target="_blank"
         rel="noreferrer"
         href="https://www.buymeacoffee.com/jmp"
@@ -23,23 +23,8 @@ const Bmc = () => {
           height="35"
           alt=""
         />
-        <span style={{ marginLeft: '5px' }}>{t('bmc.button')}</span>
+        <span className="ml-1">{t('bmc.button')}</span>
       </Link>
-      <style jsx>
-        {`
-          .bmc-button {
-            line-height: 36px;
-            height: 37px;
-            text-decoration: none;
-            display: inline-flex;
-            color: #fff;
-            background-color: #ff813f;
-            border-radius: 3px;
-            border: 1px solid transparent;
-            padding: 0 9px;
-          }
-        `}
-      </style>
     </div>
   );
 };
