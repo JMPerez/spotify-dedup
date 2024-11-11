e4b0e9b80d5b1462f4ea78ced90037d7a109edcb
José M. Pérez <JMPerez@users.noreply.github.com>
2024-11-11 11:56:06 +0100

    Add an option to decide what to do in the chain of promises in case of error

--- dedup/promiseForPages.ts
+++ dedup/promiseForPages.ts
@@ -70,6 +70,7 @@ export default async function promisesForPages(
   api: SpotifyWebApi,
   initialRequest,
   onProgressChanged: (progress: number) => void,
+  onError?: (error: Error) => boolean,
 ): Promise<Array<any>> {
   const results = await initialRequest;
   if (results === null) {
@@ -105,17 +106,30 @@ export default async function promisesForPages(
         .then((result: Array<Object>) => {
           onProgressChanged && onProgressChanged(1.0 * (++fulfilledPromises / promises.length));
           return currentPromise()
-            .then(Array.prototype.concat.bind(result))
+            .then((currentResult) => {
+              return currentResult ? result.concat(currentResult) : result;
+            })
             .catch((e) => {
               console.error('There was an error reducing promises', e);
-            })
-        }
-        )
+
+              // If onError is provided, let it decide whether to continue
+              if (onError) {
+                const shouldContinue = onError(e);
+                if (!shouldContinue) {
+                  throw e; // This will break the chain
+                }
+              }
+
+              // Continue with previous results if no onError or if it returned true
+              return result;
+            });
+        })
         .catch((e) => {
           console.error(
             'There was an error reducing promises - general catch',
             e
           );
+          throw e; // Re-throw to stop the chain if we got here
         }),
     Promise.resolve([])
   );
